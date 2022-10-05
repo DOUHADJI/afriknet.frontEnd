17687703c1b45e213907b4fb634205eec94ba154
DOUHADJI <58432265+DOUHADJI@users.noreply.github.com>
2022-10-05 09:54:24 +0000

    initialized

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+
+import { Navbar } from "@nextui-org/react"
+import { FunctionComponent } from "react"
+
+const MyNavbar : FunctionComponent = () => {
+
+    return (
+        <Navbar>
+            
+        </Navbar>
+    )
+}
+
+export default MyNavbar
